823b51990066dbaa7b074e14e5a8e29678b6a2f8
Robert Kent <robert.kent87@gmail.com>
2018-08-17 10:50:19 +0100

    Updated entry API with read method

--- objects/entry.php
+++ objects/entry.php
@@ -18,4 +18,15 @@ class Entry {
   {
     $this->conn = $db;
   }
+
+  public function read(){
+    $query = "SELECT e.id, e.date_created, e.comments
+              FROM {$this->table_name} e
+              ORDER BY e.date_created DESC";
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+
+    return $stmt;
+  }
 }
