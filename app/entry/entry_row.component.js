5bd153b551b50cbf57b2c423498bedabca183dbf
Robert Kent <robert.kent87@gmail.com>
2018-08-17 12:49:52 +0100

    Updated entry row

--- app/entry/entry_row.component.js
+++ app/entry/entry_row.component.js
@@ -2,15 +2,15 @@ class EntryRow extends React.Component {
     render() {
         return (
             <div>
-                <p>Date: {this.props.date_created}</p>
+                <p>Date: {this.props.entry.date_created}</p>
                 <div className='messages'>
-                    {this.props.messages.map((message, i) => {
+                    {this.props.entry.messages.map((message, i) => {
                         return (
                             <p><strong>{message.person}:</strong> {message.message}</p>
                         );
                     })}
                 </div>
-                <p>Comments: {this.props.comments}</p>
+                <p>Comments: {this.props.entry.comments}</p>
             </div>
         );
     }
