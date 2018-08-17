9b4ae2a6d53a6fccd1d55996a67fb882c1dc353d
Robert Kent <robert.kent87@gmail.com>
2018-08-17 11:42:07 +0100

    Set up initial index file

--- index.php
+++ index.php
@@ -1,7 +1,49 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Robert
- * Date: 17/08/2018
- * Time: 9:36 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Shit my manager says</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+
+  <!-- Custom CSS -->
+  <link href="app/assets/css/custom.css" rel="stylesheet" media="screen" />
+
+</head>
+<body>
+
+<!-- container -->
+<div class="container">
+
+  <div class="page-header">
+    <h1>Loading...</h1>
+  </div>
+
+  <!-- placeholder for rendering react components -->
+  <div id='content'></div>
+
+</div>
+<!-- /container -->
+
+<!-- react js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"></script>
+
+<!-- react code -->
+<script type="text/babel" src="app/entry/read_entries.component.js"></script>
+<script type="text/babel" src="app/main.component.js"></script>
+
+<!-- jQuery library required by bootsrap js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+</html>
