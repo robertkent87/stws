e39e8ca0576151ea2a163cf80228c0170b29ba5a
Robert Kent <robert.kent87@gmail.com>
2018-08-17 11:57:59 +0100

    Added read entries component

---
+++ app/entry/read_entries.component.js
@@ -0,0 +1,32 @@
+class ReadEntriesComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            entries: []
+        };
+    }
+
+    componentDidMount() {
+        const url = '/entry/read.php';
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => this.setState({
+                entries: data.records
+            }))
+            .catch(error => console.error(error));
+    }
+
+    render() {
+        $('.page-header h1').text('All entries');
+        return (
+            <div className='overflow-hidden'>
+                <TopActionsComponent changeAppMode={this.props.changeAppMode} />
+                <EntriesTable
+                    entries={this.state.entries}
+                    changeAppMode={this.props.changeAppMode}
+                />
+            </div>
+        );
+    }
+}
