7bcc8df326f4ea15a5eea808c0c62e445e23a0a8
Robert Kent <robert.kent87@gmail.com>
2018-08-27 11:36:00 +0100

    Updated styling for individual entries

--- app/entry/entry_row.component.js
+++ app/entry/entry_row.component.js
@@ -1,16 +1,20 @@
 class EntryRow extends React.Component {
     render() {
         return (
-            <div>
-                <p>Date: {this.props.entry.date_created}</p>
-                <div className='messages'>
-                    {this.props.entry.messages.map((message, i) => {
-                        return (
-                            <p key={i}><strong>{message.person}:</strong> {message.message}</p>
-                        );
-                    })}
+            <div className="panel panel-default">
+                <div className="panel-body">
+                    <div className='messages'>
+                        {this.props.entry.messages.map((message, i) => {
+                            return (
+                                <p key={i}><span className="label label-primary">{message.person}</span> {message.message}</p>
+                            );
+                        })}
+                    </div>
+                    <p><em>Comments: {this.props.entry.comments}</em></p>
                 </div>
-                <p>Comments: {this.props.entry.comments}</p>
+                <div className="panel-footer">
+                    Date: {this.props.entry.date_created}
+                </div>
             </div>
         );
     }
